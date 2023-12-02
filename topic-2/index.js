331d456ff46f674941fca7894412563e850231ce
websiteproduct <websiteproduct@gmail.com>
2023-12-02 02:04:48 +0100

    node package

---
+++ topic-2/index.js
@@ -0,0 +1,9 @@
+const getDetailedInfo = require("mpackage/getDetailedInfo");
+
+const user = {
+  name: "kiRiLl konySHev",
+  dateBirth: "25.04.1991",
+  purpose: "learn Node.JS",
+};
+
+console.log(getDetailedInfo(user));
---
+++ topic-2/index.mjs
@@ -0,0 +1,9 @@
+import getDetailedInfo from "mpackage/getDetailedInfo";
+
+const user = {
+  name: "kiRiLl konySHev",
+  dateBirth: "25.04.1990",
+  purpose: "learn Node.JS",
+};
+
+console.log(getDetailedInfo(user));
